44b73f8da2ddf4242ce726687a66969b298a2da6
Nicolas D <nicolas-de-saint-jean@etu.univ-lyon1.fr>
2022-11-01 22:31:31 +0100

    modif

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,15 @@
 const Discord = require('discord.js');
+const config = require('../config.js');
 
-module.exports = async function(client) {
+module.exports = async function(client, message) {
+    const status = [
+        { type: Discord.ActivityType.Streaming, url: "https://www.twitch.tv/oooooo", name: `${config.prefix}help` },
+        { type: Discord.ActivityType.Streaming, url: "https://www.twitch.tv/oooooo", name: `🔨 Actuellement en développement` },
+        { type: Discord.ActivityType.Streaming, url: "https://www.twitch.tv/oooooo", name: `${client.guilds.cache.get("1033103427137458216").memberCount} membres` }
+    ]
     console.log(`Connecté : ${client.user.tag}`);
-    client.user.setPresence({activities: [{name: '[OGORON : ACTUELLEMENT EN DEVELOPPEMENT] | /help', type: Discord.ActivityType.Streaming, url: "https://www.twitch.tv/oooooo"}], status: "dnd"});
+    setInterval(function(){
+        const index = Math.floor(Math.random() * (status.length - 1) + 1);
+        client.user.setPresence({activities: [{name: status[index].name, type: status[index].type, url: status[index].url}], status: "dnd"});
+    }, 10000);
 }
